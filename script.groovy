0444632a097be2baf2e22ddee509671e0017fd8b
omarahmedsalah33 <omarahmedsalah33@gmail.com>
2026-04-08 07:10:07 +0200

    My New Updates

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildapp(){
+    sh 'npm install'
+}
+
+def buildimage(){
+    withCredentials([usernamePassword(credentialsId:'docker_credentials',usernameVariable:'USER',passwordVariable:'PWD')]){
+      sh "echo $PWD | docker login -U $USER --password-stdin "
+      sh 'docker build -t omarsalah123/learningrepo:v1.0 .'
+      sh 'docker push omarsalah123/learningrepo:v1.0'
+    }
+}
+def testingapp(){
+    sh 'npm test'
+}
+
+def deployingapp(){
+     sh 'docker compose up'
+}
+ return this
