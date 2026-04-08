cec8c5d886a1db1f368e5c19cd7c1119177b5359
Omar Ahmed Salah Eldin <omarahmedsalah33@gmail.com>
2026-04-08 08:45:44 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildapp(){
 
 def buildimage(){
     withCredentials([usernamePassword(credentialsId:'docker_credentials',usernameVariable:'USER',passwordVariable:'PWD')]){
-      sh "echo $PWD | docker login -U $USER --password-stdin "
+      sh "echo $PWD | docker login -u $USER --password-stdin "
       sh 'docker build -t omarsalah123/learningrepo:v1.0 .'
       sh 'docker push omarsalah123/learningrepo:v1.0'
     }
